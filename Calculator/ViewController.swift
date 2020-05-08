81ce3b29d5e08782bdb71cd6c344cc10dac48022
nishitaku <29690066+nishitaku@users.noreply.github.com>
2020-05-08 14:39:17 +0900

    ラベルOutletとボタンActionの設定

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var formulaLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func inputFormula(_ sender: UIButton) {
+    }
+    @IBAction func clearCalculation(_ sender: UIButton) {
+    }
+    @IBAction func calculateAnswer(_ sender: UIButton) {
+    }
 }
 
